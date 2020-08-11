e098ccca938a6efd99aae126e5c4c8c0a0b6da89
Joe Pagan <joepagan@gmail.com>
2020-08-11 14:52:28 +0100

    Adding colours and removing icon comment from header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -220,6 +220,7 @@ export default function ButtonAppBar() {
                       target="_blank"
                       rel="noopener noreferrer"
                       className={style.navLink}
+                      color="primary"
                     >
                       <svg className={style.svg} ariaHidden="true" focusable="false" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
                         <path d="M11.56 8.87V17h8.76c1.85-.13 2.68-1.27 2.68-2.67c0-1.48-1.12-2.67-2.62-2.67c-.38 0-.7.08-1.03.22c-.24-2.34-2.23-4.17-4.68-4.17c-1.17 0-2.28.44-3.11 1.16m-.88 1.02c-.3-.18-.62-.32-.97-.39V17h1.39V9.34c-.15.16-.29.36-.42.55m-2.35-.54V17h.92V9.38c-.19-.03-.38-.04-.58-.04c-.12 0-.23 0-.34.01M6.5 10v7h.91V9.54c-.33.11-.64.27-.91.46m-1.67 2.5c-.06 0-.12-.06-.19-.09V17h.92v-6.14c-.37.48-.62 1.05-.73 1.64m-2.04-.28v4.69c.21.06.45.09.71.09h.22v-4.86c-.08-.01-.16-.02-.22-.02c-.26 0-.5.04-.71.1M1 14.56c0 .75.34 1.41.87 1.86v-3.71c-.53.44-.87 1.11-.87 1.85z" />
@@ -245,35 +246,6 @@ export default function ButtonAppBar() {
                     </Button>
                   </Tooltip>
                 </ListItem>
-                {/* <ListItem className={classes.listItem}>
-                  <Tooltip
-                    id="reverbnation-tooltip"
-                    title="Reverbnation"
-                    placement={window.innerWidth > 959 ? "top" : "left"}
-                    classes={{ tooltip: classes.tooltip }}
-                  >
-                    <Button
-
-                      href="https://www.reverbnation.com/killermantis?ref=killermantis-website"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={style.navLink}
-                    >
-                      <svg
-                        className={classes.svg}
-                        enableBackground="new 0 0 568.6 76"
-                        height="87.983047"
-                        viewBox="0 0 87.983051 87.983047"
-                        width="87.983047"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="m78.418524 32.452812-25.968893-.085424-8.200702-24.7729565-8.029855 24.7729565-25.968893.085424h.08542l20.928878 15.20547-8.115279 24.858381h.08542l21.185155-15.290894 21.014301 15.290894-8.115279-24.772957z"
-                          strokeWidth=".85424"
-                        />
-                      </svg>
-                    </Button>
-                  </Tooltip> */}
                 <ListItem className={classes.listItem}>
                   <Tooltip
                     id="patreon-tooltip"
@@ -286,6 +258,7 @@ export default function ButtonAppBar() {
                       target="_blank"
                       rel="noopener noreferrer"
                       className={style.navLink}
+                      color="primary"
                     >
                       <svg className={style.svg} ariaHidden="true" focusable="false" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet">
                         <path d="M14.82 2.41c3.96 0 7.18 3.24 7.18 7.21c0 3.96-3.22 7.18-7.18 7.18c-3.97 0-7.21-3.22-7.21-7.18c0-3.97 3.24-7.21 7.21-7.21M2 21.6h3.5V2.41H2V21.6z" />
