95448a5a1c3040c2609e0752991cb1356a277dc4
Joe Pagan <joepagan@gmail.com>
2020-08-15 13:45:36 +0100

    Removing unused page files Adding debug for dev mode

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,9 @@
+// Must be the first import
+if (process.env.NODE_ENV==='development') {
+  // Must use require here as import statements are only allowed
+  // to exist at the top of a file.
+  require("preact/debug");
+}
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
